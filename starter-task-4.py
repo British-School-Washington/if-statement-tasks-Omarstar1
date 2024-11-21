e309f6a0f48904c93f66655ab354154adc587342
Omarstar1 <omar_alomari@biswashington.org>
2024-11-21 17:49:10 +0000

    giuyeighuerwfgkureghiuefrwe

--- starter-task-4.py
+++ starter-task-4.py
@@ -3,18 +3,18 @@ score = int(input("Enter your score: "))
 
 # if the score is 90 or above
 if score >= 90:
-    # TODO: Assign the grade "A" to a variable
+    grade = "A"
 
 # if the score is between 80 and 89
 elif score >= 80:
-    # TODO: Assign the grade "B" to a variable
+    grade = "B"
 
 # if the score is between 70 and 79
 elif score >= 70:
-    # TODO: Assign the grade "C" to a variable
+    grade = "C"
 
 # if the score is below 70
 else:
-    # TODO: Assign the grade "Fail" to a variable
+    grade = "FAIL!!!!"
 
-# TODO: Print the grade variable
+print(f"You got a",grade)
